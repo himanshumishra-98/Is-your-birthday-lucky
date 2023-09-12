ffd0879b34a8a66101b8864a4a6ff896d4b818d3
Himanshu Mishra <himanshu.pmishra555@gmail.com>
2023-09-12 17:18:30 +0530

    added js functioning

---
+++ app.js
@@ -0,0 +1,36 @@
+const dateOfBirth = document.querySelector("#input-date");
+const luckyNumber = document.querySelector("#input-lucky-number");
+const chkBtn = document.querySelector("#check-button");
+const outputResult = document.querySelector("#output-result");
+
+const calculateSum = (bdate) => {
+    let sum = 0;
+    let bdate = bdate.replaceAll("-","");
+    for(let num of bdate){
+        sum += Number(num);
+    }
+    return sum;
+};
+
+const checkIsBdayLucky = (bdaySum,noToCheck) => {
+    if(bdaySum % noToCheck === 0){
+        return showMessage("Congratulations! 🥳 Your Birthdate is lucky.");
+    }
+    showMessage("Sorry! 😔 Your Birthdate is not that lucky.");
+};
+
+const showMessage = (msg) => {
+    outputResult.innerText = msg;
+};
+
+chkBtn.addEventListener("click",() => {
+    const bdate = dateOfBirth.value;
+    const noToCheck = luckyNumber.value;
+    if(bdate && noToCheck){
+        const bdaySum = calculateSum(bdate);
+        checkIsBdayLucky(bdaySum,noToCheck);
+    }else {
+        showMessage("Please enter both the fields");
+    }
+
+});
